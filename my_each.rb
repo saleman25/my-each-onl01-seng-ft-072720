6287149ec3672204dbdbbe001921c48d602fa9b8
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-30 04:44:30 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(word)
+ if block_given?
+   i = 0 
+   while i<word.length 
+   yield (word[i])
+   i = i + 1 
+ end
+ word
+ else
+   "no block was given"
+  end 
 end
